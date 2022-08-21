22287adaa52fec1cce30b6e8cbc19d5e018537f8
itadityaa <110186945+itadityaa@users.noreply.github.com>
2022-08-21 13:51:30 -0600

    Limited the ship's movement region.

--- Alien_Invasion/ship.py
+++ Alien_Invasion/ship.py
@@ -8,6 +8,7 @@ class Ship:
         """Initialize the ship and its starting position"""
         self.screen = alien_invasion.screen
         self.screen_rect = alien_invasion.screen.get_rect()
+        self.settings = alien_invasion.settings
 
         # Load the ship and get its boundaries
         self.image = pygame.image.load("images/ship.bmp")
@@ -15,6 +16,9 @@ class Ship:
 
         # Create each ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
@@ -24,10 +28,14 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on movement flag."""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        # Update the ship's x value, not the rectangle
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update the rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
