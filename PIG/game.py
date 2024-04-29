7759b5d42f481d9b0577b1cbb5e21f0dcf87abe3
itadityaa <it.adityaa@gmail.com>
2024-04-29 16:05:16 -0600

    Change definition of game

--- PIG/game.py
+++ PIG/game.py
@@ -3,12 +3,12 @@ max_score = 50
 
 def dice_roll():
     random_number = random.randint(1, 100)
-    if random_number <= 5:
+    if random_number <= 25:
         return 1
     else:
         return random.randint(2, 6)
 
-def player_turn(current_score = 0):
+def player_turn(turn_score=0):
     response = input("Would you like to roll the dice? (y/n) ")
     if response.lower() == 'y':
         roll = dice_roll()
@@ -16,20 +16,14 @@ def player_turn(current_score = 0):
             print("You rolled a 1! Your turn is over.")
             return 0
         else:
-            current_score += roll
-            print(f"You rolled a {roll}! Your score for this turn is {current_score}.\n")
-            if current_score >= max_score:
-                return current_score
-            return player_turn(current_score)
-    elif response == 'n':
-        print(f"You scored {current_score} this turn.")
-        return current_score
-
-def win_check(scores, max_score):
-    for i in range(len(scores)):
-        if scores[i] >= max_score:
-            return True, i + 1
-    return False, None
+            turn_score += roll
+            print(f"You rolled a {roll}! Your score for this turn is {turn_score}.\n")
+            if turn_score >= max_score:
+                return turn_score
+            return player_turn(turn_score)
+    elif response.lower() == 'n':
+        print(f"You scored {turn_score} this turn.")
+        return turn_score
 
 def final_score(scores):
     max_score = max(scores)
@@ -52,14 +46,12 @@ def play_game():
     scores = [0] * players
     print("The first player to reach 100 points wins!")
     print("Let's play!")
-    for i in range(players):
-        print(f"\nPlayer {i + 1}, it's your turn!")
-        scores[i] += player_turn()
-        win_condition, player_number = win_check(scores, max_score)
-        if win_condition:
-            # print(f"\nPlayer {player_number} wins! with a score of {scores[i]}")
-            break
-        print(f"\nPlayer {i + 1} your total score is {scores[i]}")
+    while max(scores) < max_score:
+        for i in range(players):
+            print(f"\nPlayer {i + 1}, it's your turn!")
+            scores[i] += player_turn()
+            
+            print(f"\nPlayer {i + 1} your total score is {scores[i]}")
     
     final_score_value, player_number = final_score(scores)
     if final_score_value == 0:
